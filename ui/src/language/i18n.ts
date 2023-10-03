378f5d19ad1000ce21e66e34ff978b6ab8415d60
fifthsegment <abdullah.irfan1@gmail.com>
2023-10-03 11:28:35 +0200

    Add locale message

--- ui/src/language/i18n.ts
+++ ui/src/language/i18n.ts
@@ -1,6 +1,7 @@
 import { init, register } from "svelte-i18n";
 
 export const setupI18n = () => {
+  console.log("[App] Setting up i18n");
   register("en", () => import("./en.json"));
   init({
     fallbackLocale: "en",
